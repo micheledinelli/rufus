e39fa2add524ff72b2b3e97aaba94be28b6058c4
micheledinelli <dinellimichele00@gmail.com>
2025-01-22 17:54:54 +0100

    feat: update Grid.tsx removing controls, adding text decoration-2 and mix-blend-difference

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -97,7 +97,7 @@ export default function Grid() {
   return (
     <div className="relative flex flex-row justify-between border-t-1 border-slate-600 h-full">
       {/* Sidebar */}
-      <div className="h-full sticky top-12 hidden text-sm md:w-1/5 md:flex md:flex-col md:justify-between md:pl-4">
+      <div className="h-full sticky top-0 hidden text-sm md:w-1/5 md:flex md:flex-col md:justify-between md:pl-4">
         <div className="font-bold mt-4">
           <p>VIDEO CREATOR</p>
           <a className="text-accent" href="mailto:info@francescorufini.it">
@@ -116,36 +116,37 @@ export default function Grid() {
           videoData &&
           videoData.map((video, index) => (
             <div key={index} className="sticky top-0 md:flex md:flex-row md:justify-start md:items-start w-full gap-8 border-b-1 border-slate-600 bg-white">
-              <div className="relative w-full h-full md:w-2/4">
+              <div className="relative w-full h-full md:w-3/5">
                 <video className="w-full h-full tv object-cover"
                   controlsList="nodownload"
                   autoPlay
                   loop
                   muted
+                  controls={false}
                   playsInline
                   poster={video.video.poster}
-                  onMouseEnter={(e) => e.currentTarget.controls = true}
-                  onMouseLeave={(e) => e.currentTarget.controls = false}
+                // onMouseEnter={(e) => e.currentTarget.controls = true}
+                // onMouseLeave={(e) => e.currentTarget.controls = false}
                 >
                   <source src={video.video.src} type={video.video.type} />
                 </video>
 
                 {/* Video badge mobile */}
-                <div className="md:hidden text-sm absolute top-4 left-4 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                <div className="md:hidden text-sm absolute top-4 left-4 text-white mix-blend-difference">
                   <a href={"/projects/" + video.id} className="font-bold">{video.title}</a>
                 </div>
-                <div className="md:hidden text-sm absolute bottom-4 right-4 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                <div className="md:hidden text-sm absolute bottom-4 right-4 text-white mix-blend-difference">
                   <p className="font-semibold">{video.role}</p>
                 </div>
               </div>
 
               {/* Video description for md and beyond  */}
-              <div className="hidden md:block mt-4 w-2/4">
-                <a href={"/projects/" + video.id} className="font-bold text-xl cursor-pointer py-2 hover:line-through decoration-4">{video.title}</a>
+              <div className="hidden md:block mt-4 w-2/5">
+                <a href={"/projects/" + video.id} className="font-bold text-xl cursor-pointer py-2 hover:line-through decoration-2">{video.title}</a>
                 <div className="hidden font-semibold md:mt-8 md:flex md:flex-col md:gap-4">
                   <div>
                     <p className="text-xs font-jetbrains">Client</p>
-                    <a href={video.client.url} className="hover:line-through decoration-4">{video.client.name}</a>
+                    <a href={video.client.url} className="hover:line-through decoration-2">{video.client.name}</a>
                   </div>
                   <div>
                     <p className="text-xs font-jetbrains">Role</p>
